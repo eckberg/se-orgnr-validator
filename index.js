671a83cba0b916671d47025ce4f36ad371c94f34
Karl Eckberg <karl@eckberg.se>
2019-08-16 11:27:04 +0200

    Code structure adjustments.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * Validates if the input is a possible Swedish company registration number
  * (organisationsnummer). Will not indicate if the number is actually
- * active or registered.
+ * active or registered. Disregards if dash is misplaced.
  *
  * @param  {string} input   The number to check, could be either a string
  *                          with or without the dash, or a number without dash.
@@ -11,46 +11,46 @@
  *                          is a valid organisationsnummer or not.
  */
 module.exports = (input) => {
-  // Only accept a string or number.
-  if (
-    typeof input !== 'string' && // Input must be either string...
-    typeof input !== 'number' // ...or number
-  ) {
+  if (typeof input === 'string') {
+    // Remove any non-digits (typically the dash)
+    const normalized = input.replace(/\D/g, '')
+    // Perform validation test
+    return validateOrgNr(normalized)
+  } else if (typeof input === 'number') {
+    // If input is a number, turn into a string
+    const normalized = '' + input
+    // Perform validation test
+    return validateOrgNr(normalized)
+  } else {
     // Throw TypeError if not string or number
     throw new TypeError(`Expected a string or number, got ${typeof input}`)
   }
+}
 
-  let normalized
-
-  // Normalize the input, if it is a string
-  if (typeof input === 'string') {
-    normalized = input
-      .replace(/\D/g, '') // Remove any non-digits (typically the dash)
-  }
-
-  // If input is a number, turn into a string
-  if (typeof input === 'number') {
-    normalized = '' + input
-  }
-
-  // Return true or false if valid or not
+/**
+ * Internal function to validate the number
+ */
+function validateOrgNr (number) {
   return (
-    normalized.length === 10 && // Length must be 10 (without dash)
-    parseInt(normalized.slice(2, 3)) >= 2 && // Third numeral must be at least 2
-    validateChecksum(normalized) // Validate the last check digit
+    // Length must be 10 (without dash)
+    number.length === 10 &&
+    // Third digit must be at least 2
+    parseInt(number.slice(2, 3)) >= 2 &&
+    // Validate the last check digit
+    validateChecksum(number)
   )
 }
 
-/*
-* Internal function to validate luhn.
-* Insipred by https://github.com/bendrucker/fast-luhn
-*/
+/**
+ * Internal function to validate the checksum.
+ * Insipred by https://github.com/bendrucker/fast-luhn
+ */
 function validateChecksum (number) {
-  var length = number.length
-  var bit = 1
-  var sum = 0
-  var value
-  var array = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
+  const array = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
+  let length = number.length
+  let bit = 1
+  let sum = 0
+  let value
 
   while (length) {
     value = parseInt(number.charAt(--length), 10)
